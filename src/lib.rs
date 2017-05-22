aa920bb63fe0205e70ff9252bf47679122c41435
Michiel Kalkman <michiel@nosuchtype.com>
2017-05-22 17:33:21 +1000

    feature: Remove embedded setup

--- src/lib.rs
+++ src/lib.rs
@@ -17,32 +17,9 @@ pub mod flox;
 #[macro_use]
 extern crate log;
 
-#[cfg(not(feature = "embedded"))]
 pub use self::xfstate::*;
-
-#[cfg(not(feature = "embedded"))]
 pub use self::xfstruct::*;
-
-#[cfg(not(feature = "embedded"))]
 pub use self::validation::*;
-
-#[cfg(not(feature = "embedded"))]
 pub use self::xfrunner::*;
-
-#[cfg(not(feature = "embedded"))]
 pub use self::dispatcher::*;
-
-#[cfg(not(feature = "embedded"))]
 pub use self::actiondispatch::*;
-
-// Embedded config
-//
-
-#[cfg(feature = "embedded")]
-extern crate libc;
-
-#[cfg(feature = "embedded")]
-mod embed;
-
-#[cfg(feature = "embedded")]
-pub use self::embed::embed::*;
