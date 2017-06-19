4b1b61f3d1c0e1265e69b73024126e5e7afb87e8
Michiel Kalkman <michiel@nosuchtype.com>
2017-06-20 06:55:39 +1000

    test: Add complex form loading

--- tests/test_structure_form.rs
+++ tests/test_structure_form.rs
@@ -7,10 +7,21 @@ mod helper;
 use helper::read_json_file;
 
 #[test]
-fn test_load_form_document() {
+fn test_load_basic_form_document() {
     let _ = env_logger::init();
 
     let json_string = read_json_file("resource/docs/form/good/basic.json");
+    let doc = FormDocument::from_json(&json_string);
+
+    println!("Loaded domain document : {:?}", doc);
+
+}
+
+#[test]
+fn test_load_nested_form_document() {
+    let _ = env_logger::init();
+
+    let json_string = read_json_file("resource/docs/form/good/nested.json");
     let doc = FormDocument::from_json(&json_string);
 
     println!("Loaded domain document : {:?}", doc);
