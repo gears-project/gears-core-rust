1a1aaf46c02689222d035ff76082e671d3a71a5d
Michiel Kalkman <michiel@nosuchtype.com>
2017-06-26 07:39:41 +1000

    test: Add model tests

---
+++ tests/test_structure_model.rs
@@ -0,0 +1,23 @@
+extern crate env_logger;
+
+extern crate xflow;
+use xflow::util::fs::*;
+use xflow::structure::model::*;
+
+#[test]
+fn test_load_model() {
+    let _ = env_logger::init();
+
+    let model_a = model_from_fs(&"resource/projects/basic").unwrap();
+    let json_a = model_a.to_json();
+    let model_b = ModelDocument::from_json(&json_a);
+    let yaml_a = model_b.to_yaml();
+    let model_c = ModelDocument::from_yaml(&yaml_a);
+
+    assert_eq!(model_a.id, model_c.id);
+    assert_eq!(model_a.doc.xflows.len(), model_c.doc.xflows.len());
+    assert_eq!(model_a.doc.forms[0].to_json(),
+               model_c.doc.forms[0].to_json());
+    assert_eq!(model_a.doc.xflows[0].to_json(),
+               model_c.doc.xflows[0].to_json());
+}
