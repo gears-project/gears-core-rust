650541e8ddcb68bcfbc0c7dc08e67d18dcbb0535
Michiel Kalkman <michiel@nosuchtype.com>
2017-07-10 18:24:32 +1000

    wip: Generate initial xflow js start body

--- src/generation/xflow_to_es5.rs
+++ src/generation/xflow_to_es5.rs
@@ -15,13 +15,13 @@ pub fn output_es5(xflow: &XFlowDocument) -> String {
 fn output_js(input_program: &str) -> String {
     let mut ast = parser::parse(input_program.to_string()).expect("Must compile");
     transformer::transform(&mut ast, transformer::Settings::no_transform());
-    codegen::generate_code(&ast, true)
+    codegen::generate_code(&ast, false)
 }
 
 fn output_js_es5(input_program: &str) -> String {
     let mut ast = parser::parse(input_program.to_string()).expect("Must compile");
     transformer::transform(&mut ast, transformer::Settings::target_es5());
-    codegen::generate_code(&ast, true)
+    codegen::generate_code(&ast, false)
 }
 
 fn build_class(xflow: &XFlowDocument) -> String {
@@ -58,7 +58,7 @@ fn local_variables(xflow: &XFlowDocument) -> String {
         .iter()
         .map({
                  |v| {
-                     format!("this.{name} = {value:?};",
+                     format!("this.input_vars.{name} = {value};",
                              name = v.name,
                              value = v.value.string_value())
                  }
@@ -77,7 +77,7 @@ fn build_nodes(xflow: &XFlowDocument) -> String {
                  |node| {
                      format!(r#" node_{id}() {{ {body} }}  "#,
                              id = node.id,
-                             body = build_function_body(&node))
+                             body = build_function_body(&node, &xflow))
                  }
              })
         .collect();
@@ -85,7 +85,39 @@ fn build_nodes(xflow: &XFlowDocument) -> String {
 
 }
 
-fn build_function_body(node: &XFlowNode) -> String {
-    "xflow_node();".to_owned()
+fn build_function_body(node: &XFlowNode, xflow: &XFlowDocument) -> String {
+    let body = match node.nodetype {
+        XFlowNodeType::Flow => build_xflow_body(&node, &xflow),
+        XFlowNodeType::Flox => "flox_node();".to_owned(),
+        XFlowNodeType::Call => "call_xflow();".to_owned(),
+    };
+
+    format!("console.log('node-{id} called'); {body}",
+            id = node.id,
+            body = body)
+}
+
+fn build_xflow_body(node: &XFlowNode, xflow: &XFlowDocument) -> String {
+
+    match node.action.as_ref() {
+        "start" => {
+            let edges = xflow.doc.get_out_edges(node);
+            match edges.len() {
+                0 => {
+                    error!("build_xflow_body: Unable to find a connecting node");
+                    format!("")
+                }
+                1 => format!("this.node_{id}();", id = edges[0].1),
+                _ => {
+                    error!("build_xflow_body: Multiple connecting nodes from start node ");
+                    format!("")
+                }
+            }
+        }
+        "end" => format!("end();"),
+        "branch" => format!("branch();"),
+        _ => format!("unimplemented();"),
+
+    }
 
 }
