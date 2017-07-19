5971f363eaf97261b1b1eb7bbdb1b6e6968a879c
Michiel Kalkman <michiel@nosuchtype.com>
2017-07-20 07:47:18 +1000

    test: Explicitly test ordered hash deserialization

--- tests/test_structure_form.rs
+++ tests/test_structure_form.rs
@@ -1,21 +1,78 @@
 extern crate env_logger;
-
+extern crate serde_json;
+extern crate uuid;
 extern crate gears;
+
 use gears::structure::page::*;
+use uuid::Uuid;
+use std::collections::HashMap;
 
 mod common;
 use common::load_doc;
 
 #[test]
-fn test_load_basic_form_document() {
+fn test_load_basic_page_document() {
     let _ = env_logger::init();
     let doc = load_doc::<PageDocument>("resource/docs/page/good/basic.json");
     let _ = format!("{:?}", doc);
 }
 
 #[test]
-fn test_load_nested_form_document() {
+fn test_load_nested_page_document() {
     let _ = env_logger::init();
     let doc = load_doc::<PageDocument>("resource/docs/page/good/nested.json");
     let _ = format!("{:?}", doc);
+}
+
+#[test]
+fn test_page_component_datatable_config_ordered_hash_deserialization() {
+    let _ = env_logger::init();
+
+    let mut dtc_a = DatatableConfig {
+        entity: "Post".to_owned(),
+        attributes: Vec::<String>::new(),
+        eventbindings: HashMap::<String, Uuid>::new(),
+    };
+
+    let mut dtc_b = dtc_a.clone();
+
+    assert_eq!(serde_json::to_string_pretty(&dtc_a).unwrap(),
+               serde_json::to_string_pretty(&dtc_b).unwrap());
+
+    let uuid_1 = Uuid::new_v4();
+    let uuid_2 = Uuid::new_v4();
+    let uuid_3 = Uuid::new_v4();
+    let uuid_4 = Uuid::new_v4();
+
+    //
+    // Insertion order of items is different for dtc_a and dtc_b
+    //
+
+    dtc_a.eventbindings.insert("1".to_owned(), uuid_1.clone());
+    dtc_a.eventbindings.insert("2".to_owned(), uuid_2.clone());
+    dtc_a.eventbindings.insert("3".to_owned(), uuid_3.clone());
+    dtc_a.eventbindings.insert("4".to_owned(), uuid_4.clone());
+
+    dtc_b.eventbindings.insert("4".to_owned(), uuid_4.clone());
+    dtc_b.eventbindings.insert("3".to_owned(), uuid_3.clone());
+    dtc_b.eventbindings.insert("2".to_owned(), uuid_2.clone());
+    dtc_b.eventbindings.insert("1".to_owned(), uuid_1.clone());
+
+    //
+    // Deserialization should be identical
+    //
+
+    assert_eq!(serde_json::to_string_pretty(&dtc_a).unwrap(),
+               serde_json::to_string_pretty(&dtc_b).unwrap());
+
+    //
+    // Also check ne condition just in case
+    //
+
+    let uuid_5 = Uuid::new_v4();
+    dtc_a.eventbindings.insert("5".to_owned(), uuid_5.clone());
+
+    assert_ne!(serde_json::to_string_pretty(&dtc_a).unwrap(),
+               serde_json::to_string_pretty(&dtc_b).unwrap());
+
 }
--- tests/test_structure_translation.rs
+++ tests/test_structure_translation.rs
@@ -2,6 +2,7 @@ extern crate env_logger;
 
 extern crate gears;
 use gears::structure::translation::*;
+use gears::structure::common::I18NString;
 
 mod common;
 use common::load_doc;
@@ -11,4 +12,47 @@ fn test_load_basic_translation_document() {
     let _ = env_logger::init();
     let doc = load_doc::<TranslationDocument>("resource/docs/translation/good/nl_NL.json");
     let _ = format!("{:?}", doc);
+}
+
+#[test]
+fn test_translation_ordered_hash_deserialization() {
+    let _ = env_logger::init();
+    let mut t_a = load_doc::<TranslationDocument>("resource/docs/translation/good/nl_NL.json");
+    let mut t_b = load_doc::<TranslationDocument>("resource/docs/translation/good/nl_NL.json");
+
+    let item_1 = I18NString {
+        locale: "nl_NL".to_owned(),
+        key: "key1".to_owned(),
+        value: "value1".to_owned(),
+    };
+
+    let item_2 = I18NString {
+        locale: "nl_NL".to_owned(),
+        key: "key2".to_owned(),
+        value: "value2".to_owned(),
+    };
+
+    let item_3 = I18NString {
+        locale: "nl_NL".to_owned(),
+        key: "key3".to_owned(),
+        value: "value3".to_owned(),
+    };
+
+    //
+    // Insert order differs between t_a and t_b
+    //
+
+    t_a.doc.items.insert(item_1.key.clone(), item_1.clone());
+    t_a.doc.items.insert(item_2.key.clone(), item_2.clone());
+    t_a.doc.items.insert(item_3.key.clone(), item_3.clone());
+
+    t_b.doc.items.insert(item_3.key.clone(), item_3.clone());
+    t_b.doc.items.insert(item_2.key.clone(), item_2.clone());
+    t_b.doc.items.insert(item_1.key.clone(), item_1.clone());
+
+    //
+    // Deserialization should be the same
+    //
+
+    assert_eq!(t_a.to_json(), t_b.to_json());
 }
