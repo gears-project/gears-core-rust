7db7ead92f648654c25fe9708e752854bcfaaac5
Michiel Kalkman <michiel@nosuchtype.com>
2017-09-22 07:18:42 +1000

    feat: Add DSL newline support

--- tests/test_dsl.rs
+++ tests/test_dsl.rs
@@ -45,41 +45,41 @@ fn test_dsl_tokens_to_tree() {
     }
 }
 
+use gears::dsl::command::{GearsDsl, DslTree, DslTokens};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Zork {
+    pub grue: bool,
+}
+
+impl Default for Zork {
+    fn default() -> Zork {
+        Zork { grue: false }
+    }
+}
+
+impl GearsDsl for Zork {
+    fn generate_dsl(&self) -> DslTokens {
+        unimplemented!();
+    }
+
+    fn consume_command(&mut self, s: &str) -> Result<(), String> {
+        unimplemented!();
+    }
+
+    fn consume_scope(&mut self, s: &str, tree: &Vec<DslTree>) -> Result<(), String> {
+        unimplemented!();
+    }
+}
+
+type ZorkList = DocumentList<Zork>;
+
 // partof: #TST-dsl-consistency-common_document_list
 //
 #[test]
 fn test_dsl_document_list() {
     let _ = env_logger::init();
 
-    use serde;
-    use gears::dsl::command::{GearsDsl, DslTree, DslTokens};
-
-    #[derive(Debug, Serialize, Deserialize)]
-    struct Zork {
-        pub grue: bool,
-    }
-
-    impl Default for Zork {
-        fn default() -> Zork {
-            Zork { grue: false }
-        }
-    }
-
-    impl GearsDsl for Zork {
-        fn generate_dsl(&self) -> DslTokens {
-            unimplemented!();
-        }
-
-        fn consume_command(&mut self, s: &str) -> Result<(), String> {
-            unimplemented!();
-        }
-
-        fn consume_scope(&mut self, s: &str, tree: &Vec<DslTree>) -> Result<(), String> {
-            unimplemented!();
-        }
-    }
-
-    type ZorkList = DocumentList<Zork>;
 
     let mut list = ZorkList::new();
 
@@ -90,6 +90,38 @@ fn test_dsl_document_list() {
     assert_eq!(list.len(), 2);
 
     assert!(list.interpret_dsl("remove itemtwo;").is_ok());
+    assert_eq!(list.len(), 1);
+
+}
+
+#[test]
+fn test_dsl_document_list_multiline() {
+    let _ = env_logger::init();
+
+
+    let mut list = ZorkList::new();
+
+    assert!(
+        list.interpret_dsl(
+            r#"
+    add itemone;
+    add itemtwo;
+    add itemthree;
+    "#,
+        ).is_ok()
+    );
+    assert_eq!(list.len(), 3);
+
+    assert!(
+        list.interpret_dsl(
+            r#"
+    remove itemone;
+    remove itemtwo;
+    remove itemthree;
+    add newitemone;
+    "#,
+        ).is_ok()
+    );
     assert_eq!(list.len(), 1);
 
 }
